cc85dc0f3f6e401eaed2d2ed051446793317333e
ArturoUrbina <61171840+ArturoUrbina@users.noreply.github.com>
2021-01-19 14:17:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ var io = require('socket.io')(server);
 var messages = [];
 
 app.use(express.static('public'));
-// el socket escuchar· el evento new-message y los traera
-// en data con el mÈtodo push
+// el socket escuchar√° el evento new-message y los traera
+// en data con el m√©todo push
 // para notificar a los clientes
 // para conectar en privado socket.emit
 // pero como es una sala de chat entonces
@@ -23,6 +23,8 @@ io.on('connection', function(socket) {
   });
 });
 
-server.listen(8080, function() {  
-  console.log("Servidor corriendo en http://localhost:8080");
+app.set('port', process.env.PORT||8080);
+
+server.listen(app.get('port'), function() {  
+  console.log("Servidor corriendo en http://localhost:"+app.get('port'));
 });
